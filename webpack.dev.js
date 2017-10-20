4454a292deea051a11ce58b8701835b2d4903a3a
Andrew Dixon <spinlock99@users.noreply.github.com>
2017-10-20 16:34:49 -0700

    updated icon

--- webpack.dev.js
+++ webpack.dev.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "NAME",
+      title: "new one",
       template: "src/index.ejs"
     }),
     new OfflinePlugin()
